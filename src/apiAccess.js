f436da6e3ef6a785e2589e9b31535ba58bf82921
JuanAntonioReyes <jantonio.reyes.navarro@gmail.com>
2018-05-22 19:09:09 +0200

    Minor changes and code cleaning

--- src/apiAccess.js
+++ src/apiAccess.js
@@ -5,27 +5,33 @@ var userToken;
 var axiosInstance = axios.create({ baseURL: config.apiUrl });
 
 export default {
+
 	fetchMessages(params) {
 		var urlGet = 'api/messages/' + params[0] + '/' + params[1]+ 
 									'/' + params[2] + '/' + params [3];
 		
 		return axiosInstance.get(urlGet);
 	},
+
 	addMessage(params) {
 		userToken = localStorage.getItem('userToken');
 		axiosInstance.defaults.headers.common['x-access-token'] = userToken;
 		return axiosInstance.post('api/messages', params);
 	},
+
 	registerUser(params) {
 		return axiosInstance.post('api/registerUser', params);
 	},
+
 	loginUser(params) {
 		return axiosInstance.post('api/loginUser', params);
 	},
+
 	getLoggedUser() {
 		userToken = localStorage.getItem('userToken');
 		axiosInstance.defaults.headers.common['x-access-token'] = userToken;
 
 		return axiosInstance.get('api/loggedUser');
-	},
+	}
+	
 }
